cd6a716139f681c82c9cc8bf4a100297ac1f1bc1
Lauri Korts-Pärn <lauri@korc.jp>
2017-12-06 00:40:43 -0500

    remove etc/rc.local, update locale.gen, bash history config

--- etc/profile.d/histfile-conf.sh
+++ etc/profile.d/histfile-conf.sh
@@ -2,6 +2,10 @@
 
 test -z "$BASH" || shopt -s histappend
 
+HISTTIMEFORMAT="%Y%m%d %T "
+HISTSIZE=50000
+HISTFILESIZE=100000
+
 test -z "$SSH_CONNECTION" ||
   case "$HISTFILE" in
     */.bash_history) HISTFILE="${HISTFILE}-${SSH_CONNECTION%% *}" ;;
